9c96adea6a75f8f046df5bcf2165fc38946d2f0a
Dariodedja <44201434+Dariodedja@users.noreply.github.com>
2018-12-29 20:57:39 +0100

    Delete SUBS.py

--- SUBS.py
+++
@@ -1,9 +0,0 @@
-def DNAsubsstring(s, t):
-    repeats = ""
-    for a in range(len(s)):
-        if s.find(t, a) == a:
-            repeats += str(a + 1) + " "
-    return repeats[:-1]
-
-
-print(DNAsubsstring("GATATATGCATATACTT", "ATAT"))
