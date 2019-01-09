4126b4e8cd06496941db5286b1680a68c2141fae
Dariodedja <44201434+Dariodedja@users.noreply.github.com>
2019-01-09 19:38:59 +0100

    Update SPLC.py

--- SPLC.py
+++ SPLC.py
@@ -12,19 +12,13 @@ def parse_fasta(filename):
     sequences.pop(0)
     return sequences
 
-
 def encoding(rna):
-    # Create a codon dictionary
     bases = ['t', 'c', 'a', 'g']
     codontable = [a + b + c for a in bases for b in bases for c in bases]
     amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
     codondict = dict(zip(codontable, amino_acids))
-
-    # Create list of codons in RNA
     rna = rna.lower()
     codonfrags = [rna[i:i + 3] for i in range(0, len(rna)-1, 3)]
-
-    # Translate RNA to protein
     protein = ""
     for codon in codonfrags:
         if codondict[codon] != '*':
@@ -33,20 +27,12 @@ def encoding(rna):
             pass
     return protein
 
-
 def main(filename):
     dnastrings = parse_fasta(filename)
-    # Set original DNA to the full list at the start
     fulldna = dnastrings[0]
-
-    # Remove the full DNA from original list
     dnastrings = dnastrings[1::]
-
-    # Remove introns from dnastring to leave exons
     for intron in dnastrings:
         fulldna = fulldna.replace(intron, "")
-
-    # Return protein encoded output
     return(encoding(fulldna))
 
 print(main("rosa.txt"))
