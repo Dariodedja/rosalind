2041c02eb204269f0583e4aedfbb5a168f0daee4
Dariodedja <44201434+Dariodedja@users.noreply.github.com>
2019-01-10 18:30:08 +0100

    Add files via upload

---
+++ LCSM.py
@@ -0,0 +1,31 @@
+def openingreadframes():
+    rosalind = open("lcsmtext.txt","r")
+    fragment = rosalind.readlines()
+    strs=[]
+    for line in fragment:
+        if line.startswith(">"):
+            strs.append("kiko")
+        else:
+            strs.append(line.rstrip("\n"))
+    rosalind.close()
+    strs = strs[1:]
+    joint = "".join(strs)
+    joint_s = joint.split("kiko")
+
+    theOne = joint_s[0]
+    rest = joint_s[1:]
+
+    start=0
+    end=2
+    matches=[]
+
+    while end < len(theOne):
+        while all(theOne[start:end] in seq for seq in rest):
+            end += 1
+        else:
+            matches.append(theOne[start:end-1])
+            end += 1
+        start = start+1
+    print(max(matches, key=len))
+
+openingreadframes()
