62ed53f730d0300cf1d6d4d17323bbd21ebf0d7d
Dario Dedja <44201434+Dariodedja@users.noreply.github.com>
2019-01-14 17:10:46 +0100

    Update TRAN.py

--- TRAN.py
+++ TRAN.py
@@ -10,6 +10,6 @@ def trans():
 
 
 if __name__ == "__main__":
-    data = open("rosalind.txt", "r").read().split('>')[1:]
+    data = open("rosalind_tran.txt", "r").read().split('>')[1:]
     dna = [''.join(x.split('\n')[1:]) for x in data]
     trans()
